c475a3747ff40f89d5fcfeef4511e3e21bca8ccf
shaheennabi <ishaheennabi333@gmail.com>
2025-05-01 10:53:21 -0700

    add

---
+++ linkedlist/remove_linkedlist_elements_5.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+        
+
+class Solution:
+    def remove_elements(self, head, val):
+        if not head:
+            return head
+        
+        fake = ListNode()
+        fake.next = head
+
+        current = fake # starting from 0 the fake node
+        
+        while current.next:  # check till fake.next exists
+            if current.next.val == val:  #if fakes next val = given val
+                current.next = current.next.next  # then currents next will ignore next node
+            else:
+                current = current.next  # this will move step by step to traverse through all the nodes
+        
+        return fake.next  # this will return from ist index
