1d3ee1d65cf2df27f8ad55e5dc3638f0c2d1eedd
shaheennabi <ishaheennabi333@gmail.com>
2025-05-14 11:51:49 -0700

    add

---
+++ stack/stack_using_array.py
@@ -0,0 +1,27 @@
+class Stack:
+
+    def __init__(self, size):
+        self.size = size 
+        self.__stack = [None] * self.size
+        self.top = -1
+
+    def push(self, value):
+        if self.top == self.size - 1:
+            return "Overflow"
+        else: 
+            self.top += 1
+            self.stack[self.top] = value
+
+    def pop(self):
+
+        if self.top == -1:
+            return  "Empty"
+        else:
+            data = self.stack[self.top]
+            self.top -= 1
+            print(data)
+
+    def traverse(self):
+
+        for i in range(self.top + 1):
+            print(self.stack[i], end=' ')
