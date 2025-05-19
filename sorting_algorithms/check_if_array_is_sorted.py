40eff1959a4269fbac0ae16eaa937ce041638d1f
shaheennabi <ishaheennabi333@gmail.com>
2025-05-19 03:34:35 -0700

    update

--- sorting_algorithms/check_if_array_is_sorted.py
+++ sorting_algorithms/check_if_array_is_sorted.py
@@ -4,7 +4,7 @@ def is_sorted(arr):
 
     sorted = True
 
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         if arr[i]>arr[i + 1]:
             sorted = False
     return sorted
